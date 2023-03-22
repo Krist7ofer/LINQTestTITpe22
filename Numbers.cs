cdc870d0f6095146d55c547bdbbee5953cd0956b
Krist7ofer <118337134+Krist7ofer@users.noreply.github.com>
2023-03-22 09:40:17 +0200

    Update Numbers.cs

--- Numbers.cs
+++ Numbers.cs
@@ -8,7 +8,7 @@ namespace LINQTestTITpe22
 {
     public class Numbers
     {       
-        public static readonly List<int> numberList = new List<int>()
+        public static readonly List<int> numbrid = new List<int>()
         {
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
         };
