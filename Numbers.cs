251fa9fe9b3bf890572a4026cd7c474e0b6a6086
opilane <opilane@HP-CZC0359NNZ>
2023-03-22 09:27:56 +0200

    Add project files.

---
+++ Numbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQTestTITpe22
+{
+    public class Numbers
+    {       
+        public static readonly List<int> numberList = new List<int>()
+        {
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        };
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace LINQTestTITpe22
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LINQ Test TITpe22");
+            Console.WriteLine("\n");
+
+            SingleOrDefaultLINQ();
+            SkipLINQ();
+        }
+
+        public static void SingleOrDefaultLINQ()
+        {
+            IList<int> oneElement = new List<int>() { 2 };
+
+            Console.WriteLine("Single or Default:");
+            //Kui nimekirjas on ainult üks element, tagastatakse see element;
+            //kui nimekirjas pole ühtegi elementi, tagastatakse null;
+            //kui nimekirjas on rohkem kui üks element, visatakse erind.
+            var singleOrDefault = oneElement.SingleOrDefault();
+                     
+            Console.WriteLine(singleOrDefault);            
+        }
+
+        public static void SkipLINQ()
+        {          
+            Console.WriteLine("\n");
+            Console.WriteLine("Skip:");
+            //Skip jätab esimesed 5 numbrit Numbers klassit vahele ja
+            //jätkab kirjutama ülejäänud numbreid          
+            var skip = Numbers.numberList.Skip(5);
+         
+            foreach (var value in skip)
+            {               
+                Console.WriteLine(value);
+            }            
+        }
+    }
+}
